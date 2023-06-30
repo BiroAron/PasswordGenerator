92e9d154e0d86849a7940b97962a89e2af3779f6
biro.aron <biro.aron000802@gmail.com>
2023-06-30 23:23:00 +0300

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int timer = 30;
+        PasswordGenerator passGenerator = new PasswordGenerator(timer);
+
+        while (true)
+        {
+            Console.WriteLine("Enter User ID:");
+            string userId = Console.ReadLine();
+
+            DateTime dateTime = DateTime.Now;
+
+            string password = passGenerator.GeneratePassword(userId, dateTime);
+            Console.WriteLine("Generated Password: " + password);
+            int i = 0;
+            bool isValid = true;
+            while (30 > i++)
+            {
+                System.Threading.Thread.Sleep(1000);
+                isValid = passGenerator.IsPasswordValid(userId, dateTime, password);
+                if (isValid)
+                {
+                    Console.WriteLine("You have " + (30 - i) + " seconds left to use this password.");
+                }
+                else
+                {
+                    Console.WriteLine("Password is no longer valid.");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PasswordGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class PasswordGenerator
+{
+    private int timer;
+    public PasswordGenerator(int timer)
+    {
+        this.timer = timer;
+    }
+
+    public string GeneratePassword(string userId, DateTime dateTime)
+    {
+        string combinedString = $"{userId}_{dateTime.ToString("yyyyMMddHHmmss")}";
+        byte[] byteData = System.Text.Encoding.UTF8.GetBytes(combinedString);
+        byte[] hashBytes;
+        using (System.Security.Cryptography.SHA256 sha256 = System.Security.Cryptography.SHA256.Create())
+        {
+            hashBytes = sha256.ComputeHash(byteData);
+        }
+        int passwordLength = 6;
+        string password = BitConverter.ToString(hashBytes, 0, passwordLength).Replace("-", "");
+        return password;
+    }
+
+    public bool IsPasswordValid(string userId, DateTime dateTime, string password)
+    {
+        string generatedPassword = GeneratePassword(userId, dateTime);
+        bool isValid = string.Equals(password, generatedPassword, StringComparison.OrdinalIgnoreCase);
+        DateTime currentDateTime = DateTime.Now;
+        bool isExpired = currentDateTime > dateTime.AddSeconds(timer);
+        return isValid && !isExpired;
+    }
+}
